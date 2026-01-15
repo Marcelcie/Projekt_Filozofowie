46c276ad413bfe017c01a3bc0be38a5415fcf773
Marcel <cieslinski.marcel@gmail.com>
2026-01-15 11:59:28 +0000

    added GUI

--- main.cpp
+++ main.cpp
@@ -1,32 +1,37 @@
 #include <iostream> 
 #include <pthread.h>
 #include <unistd.h>
-#include <ncurses.h>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
+#include <cmath>
+#include <SFML/Graphics.hpp>
 
 const int ilosc_filozofow = 5;
-pthread_mutex_t widelec[ilosc_filozofow];
-pthread_mutex_t mutex_stan = PTHREAD_MUTEX_INITIALIZER; 
-enum Stan { MYSLI, GLODNY, Je_salatke_grecka };
-Stan stan[ilosc_filozofow];
-const char* nazwa_stanu[] = { "MYSLI", "GLODNY", "Je_salatke_grecka" };
 volatile bool dziala = true;
+
+
+enum Stan { MYSLI, GLODNY, JE_SALATKE };
+Stan stan[ilosc_filozofow];
+
+pthread_mutex_t widelec[ilosc_filozofow];
+pthread_mutex_t mutex_stan = PTHREAD_MUTEX_INITIALIZER;
 
 void* filozof(void* arg) {
     int id = *(int*)arg; 
     delete (int*)arg;
     int lewy_widelec = id;
     int prawy_widelec = (id + 1) % ilosc_filozofow;
+
     while (dziala) {
         pthread_mutex_lock(&mutex_stan);
         stan[id] = MYSLI;    
         pthread_mutex_unlock(&mutex_stan);         
-        usleep(500000 + rand() % 500000); 
+        usleep(1000000 + rand() % 2000000);
+
         pthread_mutex_lock(&mutex_stan);
         stan[id] = GLODNY;    
         pthread_mutex_unlock(&mutex_stan); 
+
         if (id % 2 == 0) {
             pthread_mutex_lock(&widelec[lewy_widelec]);
             pthread_mutex_lock(&widelec[prawy_widelec]);
@@ -34,58 +39,121 @@ void* filozof(void* arg) {
             pthread_mutex_lock(&widelec[prawy_widelec]);
             pthread_mutex_lock(&widelec[lewy_widelec]);
         }
+
         pthread_mutex_lock(&mutex_stan);
-        stan[id] = Je_salatke_grecka;    
+        stan[id] = JE_SALATKE;    
         pthread_mutex_unlock(&mutex_stan); 
-        usleep(800000 + rand() % 500000);
+        usleep(1500000 + rand() % 1000000);
+
         pthread_mutex_unlock(&widelec[prawy_widelec]);
         pthread_mutex_unlock(&widelec[lewy_widelec]);
     }
-
     return NULL;
 }
 
 int main() {
     srand(time(NULL));
-    initscr();
-    noecho();
-    curs_set(0);
-    nodelay(stdscr, TRUE);
+
     for (int i = 0; i < ilosc_filozofow; i++) {
         pthread_mutex_init(&widelec[i], NULL);
         stan[i] = MYSLI;
     }
-    pthread_t watki[ilosc_filozofow];       
 
+    pthread_t watki[ilosc_filozofow];       
     for (int i = 0; i < ilosc_filozofow; i++) {
         int* id_pwt = new int(i);
         pthread_create(&watki[i], NULL, filozof, id_pwt);
     }
-    while (dziala) {
-        clear();
-        mvprintw(0, 0, "=== Uczta Filozofow ===");
-        mvprintw(1, 0, "Nacisnij 'q' zeby zakonczyc.");
 
-        pthread_mutex_lock(&mutex_stan);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Problem Ucztujacych Filozofow - GUI", sf::Style::Default, settings);
+    window.setFramerateLimit(60);
+
+    sf::CircleShape stol(150.f);
+    stol.setFillColor(sf::Color(60, 60, 60));
+    stol.setOutlineThickness(5);
+    stol.setOutlineColor(sf::Color::White);
+    stol.setOrigin(150.f, 150.f);
+    stol.setPosition(400.f, 300.f);
+
+    sf::Font font;
+    if (!font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf")) {
+    std::cout << "Błąd ładowania czcionki!" << std::endl;
+}
+    while (window.isOpen() && dziala) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                dziala = false;
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(30, 30, 30));
+        window.draw(stol);
+
         for (int i = 0; i < ilosc_filozofow; i++) {
-            mvprintw(i + 3, 0, "Filozof %d: %s", i, nazwa_stanu[stan[i]]);
-        }
-        pthread_mutex_unlock(&mutex_stan);
-        refresh();
-        usleep(100000);
+            float angle = i * 2 * M_PI / ilosc_filozofow;
+            float x = 400.f + 230.f * cos(angle);
+            float y = 300.f + 230.f * sin(angle);
 
-        int klawisz = getch();
-        if (klawisz == 'q'){
-            dziala = false;
+            sf::Color kolorTulowia;
+            pthread_mutex_lock(&mutex_stan);
+            if (stan[i] == JE_SALATKE) kolorTulowia = sf::Color::Green;
+            else if (stan[i] == MYSLI) kolorTulowia = sf::Color::Blue;
+            else kolorTulowia = sf::Color::Red;
+            pthread_mutex_unlock(&mutex_stan);
+
+            sf::RectangleShape tulow(sf::Vector2f(50.f, 50.f));
+            tulow.setFillColor(kolorTulowia);
+            tulow.setOutlineThickness(2);
+            tulow.setOutlineColor(sf::Color::White);
+            tulow.setOrigin(25.f, 25.f);
+            tulow.setPosition(x, y);
+            tulow.setRotation(angle * 180 / M_PI + 90);
+
+            sf::CircleShape glowa(15.f);
+            glowa.setFillColor(sf::Color(255, 239, 213));
+            glowa.setOutlineThickness(2);
+            glowa.setOutlineColor(sf::Color::Black);
+            glowa.setOrigin(15.f, 15.f);
+            glowa.setPosition(x, y);
+
+            window.draw(tulow);
+            window.draw(glowa);
         }
+        // --- LEGENDA ---
+            std::vector<std::pair<sf::Color, std::string>> legenda = {
+                {sf::Color::Blue, "Mysli"},
+                {sf::Color::Red, "Glodny (czeka)"},
+                {sf::Color::Green, "Je salatke"}
+            };
+
+            for (int i = 0; i < legenda.size(); i++) {
+                sf::CircleShape colCircle(10.f);
+                colCircle.setFillColor(legenda[i].first);
+                colCircle.setPosition(20.f, 20.f + (i * 30.f));
+                window.draw(colCircle);
+
+                sf::Text text;
+                text.setFont(font);
+                text.setString(legenda[i].second);
+                text.setCharacterSize(15);
+                text.setFillColor(sf::Color::White);
+                text.setPosition(50.f, 18.f + (i * 30.f));
+                window.draw(text);
+            }
+            // --- KONIEC LEGENDY ---
+
+        window.display();
     }
-
+    dziala = false;
     for (int i = 0; i < ilosc_filozofow; i++) {
         pthread_join(watki[i], NULL);
         pthread_mutex_destroy(&widelec[i]);
     }
     pthread_mutex_destroy(&mutex_stan);
-    
-    endwin();
+
     return 0;
 }
